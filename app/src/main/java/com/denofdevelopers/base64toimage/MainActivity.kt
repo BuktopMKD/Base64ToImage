b3dc8ea34ccc67fefc46455f57e4c7ae29715a6c
Viktor Jovanovski <BuktopMKD@gmail.com>
2019-12-27 11:06:19 +0100

    Initial Commit

---
+++ app/src/main/java/com/denofdevelopers/base64toimage/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.denofdevelopers.base64toimage
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        image.setImageBitmap(Base64Util.convertStringToBitmap(getString(R.string.helmets)))
+    }
+}
